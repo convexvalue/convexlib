e3ab16d79b5de5c8164f8185b74e334d7e075a59
jbtobar <jbtobar.io@gmail.com>
2024-04-07 07:31:00 -0500

    route fix

--- convexlib/api.py
+++ convexlib/api.py
@@ -2,20 +2,14 @@ import requests
 
 class ConvexApi:
     def __init__(self, email, password, kind = ""):
-        # self.data_routes = "/api/v8/alloy"
 
-        if kind == "pro":
-            self.base_url = f"https://pro.convexvalue.com"
-        elif kind == "live":
-            self.base_url = f"https://live.convexvalue.com"
-        else:
-            self.base_url = f"https://convexvalue.com"
+        self.base_url = f"https://convexvalue.com"
 
         self.session = requests.Session()
         self.login(email, password)
 
     def login(self, email, password):
-        login_url = f"{self.base_url}/api/v8/user/login"
+        login_url = f"{self.base_url}/api/access/login"
         login_data = {"email": email, "password": password}
 
         response = self.session.post(login_url, json=login_data)
@@ -27,8 +21,8 @@ class ConvexApi:
             response.raise_for_status()
 
     def get_und(self,symbols,params):
-        data = {"symbols":symbols,"params":params,"limit":0}
-        return self.make_request(endpoint="/api/v8/alloy/core/data/und",method="POST",data=data)
+        data = [{"Und":{"s":symbols,"v":params}}]
+        return self.make_request(endpoint="/api/core/get",method="POST",data=data)
 
     def get_chain(self,root,params,exps = None, rng = None):
         data = {
@@ -37,22 +31,26 @@ class ConvexApi:
             "exps": exps,
             "rng": rng
         }
-        return self.make_request(endpoint="/api/v8/alloy/core/data/chain",method="POST",data=data)
+        return self.make_request(endpoint="/api/core/get/chain",method="POST",data=data)
 
     def get_chain_as_rows(self,root,params,exps = None, rng = None):
         res = self.get_chain(root,params,exps,rng)
         rows = []
         for element in res['data'][0]['chain']:
-            exp = element['exp'];
-            am = element['am'];
-            for value in element['stks']:
-                callitem = [value['call'][0],exp,value['stk'],'call']
-                for v in value['call'][1]:
-                    callitem.append(v)
+            exp = element[0];
+            for value in element[1]:
+                stk = value[0]
+                call = value[1]
+                put = value[2]
+                callitem = [call[0],exp,stk,'call']
+                for ix,v in enumerate(call):
+                    if ix > 0:
+                        callitem.append(v)
 
-                putitem = [value['put'][0],exp,value['stk'],'put']
-                for v in value['put'][1]:
-                    putitem.append(v)
+                putitem = [put[0],exp,stk,'put']
+                for ix,v in enumerate(put):
+                    if ix > 0:
+                        putitem.append(v)
 
                 rows.append(callitem)
                 rows.append(putitem)
